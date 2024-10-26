0d80181c09593dd7d1656edd5b25b9e4487a141a
Josue Patricio <josretamozovar@gmail.com>
2024-10-25 19:29:56 -0500

    first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>Hola soy el Home</div>
+  )
+}
+
+export default Home
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
